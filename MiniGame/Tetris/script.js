52d57dc511e0da226934c6bb30c182bcfff94137
박소연 <144569130+ParkSoYeon666@users.noreply.github.com>
2024-09-06 13:50:51 +0900

    Update script.js

--- MiniGame/Tetris/script.js
+++ MiniGame/Tetris/script.js
@@ -2,13 +2,13 @@ const canvas = document.getElementById('gameCanvas');
 const context = canvas.getContext('2d');
 const grid = 32;
 const tetrominoes = [
-    [1, 1, 1, 1], // I
-    [1, 1, 1, 0, 1], // T
-    [1, 1, 0, 0, 1, 1], // S
-    [0, 1, 1, 1, 1], // Z
-    [1, 1, 1, 0, 0, 1], // L
-    [1, 1, 1, 0, 1], // J
-    [1, 1, 1, 1] // O
+    [1, 1, 1, 1],
+    [1, 1, 1, 0, 1],
+    [1, 1, 0, 0, 1, 1],
+    [0, 1, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1],
+    [1, 1, 1, 0, 1],
+    [1, 1, 1, 1]
 ];
 
 let tetromino = tetrominoes[0];
